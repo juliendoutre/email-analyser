548589f761004bd8dc7ef6ddcd6bff03ec02a797
Julien 'Orphus' Doutre <julien.doutre@student.ecp.fr>
2020-04-07 19:19:46 +0200

    Remove beta

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Header, Icon, Label } from 'semantic-ui-react';
+import { Header, Icon } from 'semantic-ui-react';
 
 export default () => (
   <Header as="h1" icon textAlign="center">
     <Icon name="mail" circular />
     <Header.Content>
       Email Analyser
-      <Label color="blue">
-        BETA
-      </Label>
     </Header.Content>
     <Header.Subheader>
       Parse an email headers to get more insights into its metadata.
